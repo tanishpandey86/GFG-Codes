498b618185acf0a718f94ff7fd7ea7889895880f
Tanish Pandey <tanishpandey86@gmail.com>
2024-10-16 23:24:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+public:
+    int countMin(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        // Fill the table. The gap represents the length of the substring.
+        for (int gap = 1; gap < n; ++gap) {
+            for (int l = 0, r = gap; r < n; ++l, ++r) {
+                if (s[l] == s[r]) {
+                    dp[l][r] = dp[l + 1][r - 1];
+                } else {
+                    dp[l][r] = min(dp[l + 1][r], dp[l][r - 1]) + 1;
+                }
+            }
+        }
+
+        // The answer for the whole string is in dp[0][n-1]
+        return dp[0][n - 1];
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.countMin(str) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
