458dd1f101fb358b923c7b83831a065f4704f090
Tanish Pandey <tanishpandey86@gmail.com>
2024-10-31 09:45:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.cpp
@@ -0,0 +1,173 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* buildList() {
+    vector<int> arr;
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    int number;
+    while (ss >> number) {
+        arr.push_back(number);
+    }
+    if (arr.empty()) {
+        return NULL;
+    }
+    int val = arr[0];
+    int size = arr.size();
+
+    Node* head = new Node(val);
+    Node* tail = head;
+
+    for (int i = 1; i < size; i++) {
+        val = arr[i];
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+void printList(Node* n) {
+    while (n) {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+
+// } Driver Code Ends
+/* node for linked list:
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    private:
+    Node* reverse(Node* head) {
+        
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* next = NULL;
+        
+        while(curr != NULL) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    void insertAtTail(struct Node* &head, struct Node* &tail, int val) {
+        
+        Node* temp = new Node(val);
+        //empty list
+        if(head == NULL) {
+            head = temp;
+            tail = temp;
+            return;
+        }
+        else
+        {
+            tail -> next = temp;
+            tail = temp;
+        }
+    }
+    
+    struct Node* add(struct Node* first, struct Node* second) {
+        int carry = 0;
+        
+        Node* ansHead = NULL;
+        Node* ansTail = NULL;
+        
+        while(first != NULL || second != NULL || carry != 0) {
+            
+            int val1 = 0;
+            if(first != NULL)
+                val1 = first -> data;
+                
+            int val2 = 0;
+            if(second !=NULL)
+                val2 = second -> data;
+            
+            
+            int sum = carry + val1 + val2;
+            
+            int digit = sum%10;
+            
+            //create node and add in answer Linked List
+            insertAtTail(ansHead, ansTail, digit);
+            
+            carry = sum/10;
+            if(first != NULL)
+                first = first -> next;
+            
+            if(second != NULL)
+                second = second -> next;
+        }
+        return ansHead;
+    }
+    public:
+    //Function to add two numbers represented by linked list.
+    struct Node* addTwoLists(struct Node* first, struct Node* second)
+    {
+        //step 1 -  reverse input LL
+        first = reverse(first);
+        second = reverse(second);
+        
+        //step2 - add 2 LL
+        Node* ans = add(first, second);
+        
+        //step 3 
+        ans = reverse(ans);
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To ignore the newline character after the integer input
+
+    while (t--) {
+        Node* num1 = buildList();
+        Node* num2 = buildList();
+        Solution ob;
+        Node* res = ob.addTwoLists(num1, num2);
+        printList(res);
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
