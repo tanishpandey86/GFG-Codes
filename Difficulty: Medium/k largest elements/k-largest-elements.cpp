a92ff8befeeaf4592d86e3da79d83a54686437da
Tanish Pandey <tanishpandey86@gmail.com>
2024-12-01 22:28:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/k largest elements/k-largest-elements.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    vector<int> kLargest(vector<int>& arr, int k) {
+        // code here
+        vector<int> ans;
+        
+        if(k > arr.size())
+        return ans;
+        
+        sort(arr.begin(), arr.end());
+        reverse(arr.begin(), arr.end());
+        
+        int i = 0;
+        while(i < k){
+            ans.push_back(arr[i++]);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.kLargest(arr, k);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
