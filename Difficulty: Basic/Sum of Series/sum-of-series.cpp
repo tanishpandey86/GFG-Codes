055df748885fdea3f4c6bd0835154c4f2d7f0708
Tanish Pandey <tanishpandey86@gmail.com>
2024-12-20 22:22:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Series/sum-of-series.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long seriesSum(int n) {
+        // code here
+        long long sum=0;
+        for(int i = 0;i<=n;i++){
+            sum = sum + i;
+        }
+        return sum;
+    }
+}; 
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        Solution obj;
+        long long res = obj.seriesSum(n);
+
+        cout << res << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
