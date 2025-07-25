621f8accd34f06144f0565edc92c6b94fe1d2354
Tanish Pandey <tanishpandey86@gmail.com>
2025-07-26 01:46:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element II/majority-element-ii.cpp
+++ Difficulty: Medium/Majority Element II/majority-element-ii.cpp
@@ -1,65 +1,20 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-public:
+  public:
     vector<int> findMajority(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> result;
-        sort(arr.begin(), arr.end());
-        
-        int OccuranceCount = 1;
-        int prev = arr[0];
-        
-        for(int i = 1; i < n; i++) {
-            if(arr[i] == prev) {
-                OccuranceCount++;
-            } else {
-                if(OccuranceCount > n/3) {
-                    result.push_back(prev);
-                }
-                OccuranceCount = 1;
-                prev = arr[i];
+        // Code here
+        vector<int> ans;
+        map<int, int> mp;
+        for(auto it : arr)
+        {
+            mp[it]++;
+        }
+        for(auto it : mp)
+        {
+            if(it.second > arr.size() / 3)
+            {
+                ans.push_back(it.first);
             }
         }
-        
-        // Check for the last element
-        if(OccuranceCount > n/3) {
-            result.push_back(prev);
-        }
-        
-        return result;
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-
-        string s;
-        getline(cin, s);
-        stringstream ss(s);
-        vector<int> nums;
-        int num;
-        while (ss >> num) {
-            nums.push_back(num);
-        }
-        Solution ob;
-        vector<int> ans = ob.findMajority(nums);
-        if (ans.empty()) {
-            cout << "[]";
-        } else {
-            for (auto &i : ans)
-                cout << i << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
