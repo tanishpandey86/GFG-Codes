034d4374e1d5f816e6d7fa5db95629dad4f9228a
Tanish Pandey <tanishpandey86@gmail.com>
2025-08-06 10:49:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.cpp
+++ Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.cpp
@@ -1,55 +1,24 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function template for C++
-
 class Solution {
   public:
-    int romanToDecimal(string &str) {
-        // code here
-        int n = str.length();  
-        int total = 0;       
-        auto getValue = [](char c) -> int {  
-            if (c == 'I') return 1;
-            if (c == 'V') return 5;
-            if (c == 'X') return 10;
-            if (c == 'L') return 50;
-            if (c == 'C') return 100;
-            if (c == 'D') return 500;
-            if (c == 'M') return 1000;
-            return 0; 
-        };
-
-        for (int i = 0; i < n; i++) {
-            int currentValue = getValue(str[i]);
-
-            if (i < n - 1 && currentValue < getValue(str[i + 1])) {
-                total -= currentValue;
+    int romanToDecimal(string &s) {
+        int ans = 0;
+        map<char,int>val;
+        val['I'] = 1;
+        val['V'] = 5;
+        val['X'] = 10;
+        val['L'] = 50;
+        val['C'] = 100;
+        val['D'] = 500;
+        val['M'] = 1000;
+        for(int i=0;i<s.size();i++){
+            if(i+1 < s.size() and val[s[i+1]] > val[s[i]]){
+                ans += val[s[i+1]]-val[s[i]];
+                i++;
             }
-            else {
-                total += currentValue;
+            else{
+                ans += val[s[i]];
             }
         }
-
-        return total;
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-        Solution ob;
-        cout << ob.romanToDecimal(s) << endl;
-    }
-}
-// } Driver Code Ends
