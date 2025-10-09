9c220e6716b843e0f3a09c10b338ffcc2788a5d4
Tanish Pandey <tanishpandey86@gmail.com>
2025-10-09 16:00:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Postorder Traversal/postorder-traversal.cpp
@@ -0,0 +1,34 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void traverse(Node* root, vector<int>& ans) {
+        if (root == NULL) return;
+
+        // 1. Visit left subtree
+        traverse(root->left, ans);
+        // 2. Visit right subtree
+        traverse(root->right, ans);
+        // 3. Visit the node itself
+        ans.push_back(root->data);
+    }
+
+    vector<int> postOrder(Node* root) {
+        vector<int> ans;
+        traverse(root, ans);
+        return ans;
+    }
+};
